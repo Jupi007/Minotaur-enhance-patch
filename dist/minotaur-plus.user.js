53d7cc84cd5739ccbd17f9f628e3ea8448c2d0cf
Paul Kepinski <paul.kepinski@orange.fr>
2025-06-16 22:39:47 +0200

    Add icon to meta

--- dist/minotaur-plus.user.js
+++ dist/minotaur-plus.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
-// @name         Minotaur hack
-// @version      2025-03-30
+// @name         Minotaur enhance patch
+// @icon         https://minotaur.sso.gendarmerie.interieur.gouv.fr/v2/favicons/favicon.svg
+// @version      2025-06-16
 // @grant        GM.xmlHttpRequest
 // @require      https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js
 // @match        https://minotaur.sso.gendarmerie.interieur.gouv.fr/*
