c21fa09ea02df1ca51f23c8720dd9d5f5c8d155d
Paul Kepinski <paul.kepinski@orange.fr>
2025-06-19 16:41:21 +0200

    Add update-url

--- minotaur-enhance-patch.meta.js
+++ minotaur-enhance-patch.meta.js
@@ -3,6 +3,7 @@
 // @description  Améliore l'interface utilisateur de Minot@ur
 // @icon         https://www.interieur.gouv.fr/themes/custom/dsfr/src/images/logo/favicon.png
 // @version      2025-06-19
+// @updateURL    https://github.com/Jupi007/Minotaur-enhance-patch/raw/refs/heads/main/dist/minotaur-enhance-patch.user.js
 // @grant        GM.xmlHttpRequest
 // @require      https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js
 // @match        https://minotaur.sso.gendarmerie.interieur.gouv.fr/*
